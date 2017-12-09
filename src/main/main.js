2ea69518bd50e91713f18d010b756b3e229bd059
Evan Xie <dreamcoding@outlook.com>
2017-12-10 01:36:46 +0800

    Finish showing index.html

---
+++ src/main/main.js
@@ -0,0 +1,80 @@
+// Basic parameters
+const electron = require('electron')
+const BrowserWindow = electron.BrowserWindow
+const app = electron.app
+const path = require('path')
+
+var projectPath = path.join(__dirname, '/../..')
+var mainWindow = null
+
+//if (process.mas) app.setName('Electron APIs')
+app.setName("Missile Expert System")
+
+switch (process.argv[1]) {
+    default:
+        console.log('hahah')
+        initialize()
+}
+
+
+function initialize () {
+  var shouldQuit = makeSingleInstance()
+  if (shouldQuit) return app.quit()
+
+  app.on('ready', function () {
+    createWindow()
+  })
+
+  app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+
+  app.on('activate', function () {
+    if (mainWindow === null) {
+      createWindow()
+    }
+  })
+}
+
+// Make this app a single instance app.
+//
+// The main window will be restored and focused instead of a second window
+// opened when a person attempts to launch a second instance.
+//
+// Returns true if the current version of the app should quit instead of
+// launching.
+function makeSingleInstance () {
+  if (process.mas) return false
+
+  return app.makeSingleInstance(function () {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+}
+
+
+function createWindow () {
+    var windowOptions = {
+        width: 1080,
+        minWidth: 680,
+        height: 840,
+        title: app.getName()
+    }
+
+    mainWindow = new BrowserWindow(windowOptions)
+    //mainWindow.loadURL("https://baidu.com")
+    mainWindow.loadURL(path.join("file://", projectPath, '/app/pages/index.html'))
+    //mainWindow.show()
+    //mainWindow.once('ready-to-show', () => {mainWindow.show()})
+
+
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+
+
