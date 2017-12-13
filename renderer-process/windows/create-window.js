c68f35ca5a37c15c6721afb8bb19df3c0e1dd270
Evan Xie <dreamcoding@outlook.com>
2017-12-13 23:58:06 +0800

    Delete some useless file such as *.js

--- renderer-process/windows/create-window.js
+++
@@ -1,8 +0,0 @@
-const BrowserWindow = require('electron').remote.BrowserWindow
-const path = require('path')
-
-const newWindowBtn = document.getElementById('new-window')
-
-newWindowBtn.addEventListener('click', function (event) {
-
-})
